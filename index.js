314ff706dee6235dab87d2f0741c78bc8e11d9c3
Leonardo Machado <leonardoms@id.uff.br>
2021-10-16 06:02:19 -0300

    route test

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/about', (req, res) => {
+  res.send('about');
+});
+
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
