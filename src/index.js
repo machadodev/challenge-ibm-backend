aa740d3c3911a44c2352a3d9285098015443ac2f
Leonardo Machado <leonardoms@id.uff.br>
2021-10-16 06:29:57 -0300

    source formated with prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/about', (req, res) => {
-  res.send('about')
-})
+  res.send('about');
+});
 
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`)
-})
+  console.log(`App listening at http://localhost:${PORT}`);
+});
