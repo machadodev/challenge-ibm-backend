6fd82a617abe2647ea61010980ec5a1b00288063
Leonardo Machado <leonardoms@id.uff.br>
2021-10-16 06:18:07 -0300

    eslint, prettier, sucrase setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+  res.send('about')
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+})
