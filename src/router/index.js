560c1cc11a5969c9a7bd7a34e06ce670b8144f9c
Leonardo Machado <leonardoms@id.uff.br>
2021-10-16 11:03:51 -0300

    api docs first

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,6 @@ const swaggerFile = require('../docs/swagger_output.json');
 const HealthController = require('../app/controllers/health-controller');
 
 module.exports = function (app) {
-  app.get('/', HealthController.getHealth);
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+  app.get('/', HealthController.getHealth);
 };
