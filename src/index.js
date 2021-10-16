f22d2916a9f2a0154bfffc035bfcb5fdc7810a54
Leonardo Machado <leonardoms@id.uff.br>
2021-10-16 07:12:21 -0300

    add morgan

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
+app.use(morgan('combined'));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
