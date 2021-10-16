27e648f0d8742a03428065fddd40f113aab504df
Leonardo Machado <leonardoms@id.uff.br>
2021-10-16 07:10:19 -0300

    add helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
